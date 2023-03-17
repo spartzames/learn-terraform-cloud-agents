9919bc1b2b6f8d164f9472500eca051153bc5315
spartzames <spartzames@gmail.com>
2023-03-17 13:28:14 +0900

    edit main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,24 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
+    required_providers {
+        docker = {
+            source = "kreuzwerker/docker"
+            version = "~> 2.13.0"
+        }
     }
-  }
 }
 
 provider "docker" {}
 
 resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
+    name = "nginx:latest"
+    keep_locally = false
 }
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "nginx"
-  ports {
-    internal = 80
-    external = 8000
-  }
+    image = docker_image.nginx.latest
+    name = "terraform-nginx-demo"
+    ports {
+        internal = 80
+        external = 8000
+    }
 }
